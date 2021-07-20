db6981cb58ae53f9600e5c66dbbdd49738115df1
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-07-21 00:48:25 +0300

    Project was created. Styled-components, axios and airbnb-eslint were installed to project. Api and components were created. CountriesApi.jsx was coded.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CountriesApi from './api/CountriesApi';
+
+function App() {
+  return (
+    <div>
+      Hello
+      <CountriesApi />
+    </div>
+  );
+}
+
+export default App;
