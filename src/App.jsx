58657f583a9889e7910698c6ca580c4560c27489
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-07-22 10:18:28 +0300

    Layouts folder was designed. Header component was moved to components folder. App.jsx was designed for layouts folder.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-// import CountryList from './components/CountryList/CountryList';
-import Header from './layouts/Header/Header';
-import SpokenLanguages from './components/SpokenLanguages/SpokenLanguages';
+import CountryList from './components/CountryList/CountryList';
+import Layout from './layouts/Layout/Layout';
+import Main from './layouts/Main/Main';
+// import SpokenLanguages from './components/SpokenLanguages/SpokenLanguages';
 
 function App() {
   return (
-    <>
-      <Header />
-      {/* <CountryList /> */}
-      <SpokenLanguages />
-    </>
+    <Layout>
+      <Main>
+        <CountryList />
+        {/* <SpokenLanguages /> */}
+      </Main>
+    </Layout>
   );
 }
 
